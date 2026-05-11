d7fc720eb9df6546e593417493e655232387e0ee
Jaime <duckroman@gmail.com>
2026-05-11 13:47:39 -0600

    fix: Se corrigió error al cargar la aplicación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,7 +86,6 @@ function App() {
                 <span title={activeFile}>{activeFile}</span>
               </div>
             )}
-          </div>
         </div>
 
         <div className="sidebar-section">
